29316e2d003edb897623c2da125c791bd1dda75c
GREICI SOUZA <greicisouza@hotmail.com>
2020-03-28 17:42:10 -0300

    #51 Adição de validações na classe Sessao

--- CineManager/CineManager/Models/Sessao.cs
+++ CineManager/CineManager/Models/Sessao.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,13 +10,25 @@ namespace CineManager.Models
     public class Sessao
     {
         public int Id { get; set; }
-        [Required]
+      
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Duração da Sessão")]
+        [Column(TypeName = "int")]
         public int Duracao_Sessao { get; set; }
-        [Required]
-        public string Filme { get; set; }
-        [Required]
-        public int Sala { get; set; }
 
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [RegularExpression(@"^[A-Z0-9]+[a-zA-Z0-9""'\s-]*$^ ")]
+        [MaxLength(200, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        [MinLength(1, ErrorMessage = "O campo {0} deve ter no mínimo {1} caractere")]
+        [Column(TypeName = "varchar(200)")]
+
+        public string Filme { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [RegularExpression(@"^[0-9]*$")]
+        [Column(TypeName = "int")]
+        public int Sala { get; set; }
 
     }
 }
