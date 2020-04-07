e13900b79db3eaccae4d2aa7fa96e06ac7fccd5e
Vitor Hainosz <2019203186@unibrasil.com.br>
2020-04-06 23:35:54 -0300

    Alterando as views e controller do Filme

--- CineManager/CineManager/Controllers/FornecedorController.cs
+++ CineManager/CineManager/Controllers/FornecedorController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CineManager.Data;
 using CineManager.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CineManager.Controllers
 {
+    [Authorize(Policy = "CineManeger")]
     public class FornecedorController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -24,24 +26,6 @@ namespace CineManager.Controllers
         {
             return View(await _context.Fornecedor.Include(x => x.Email).
                 Include(x => x.Telefone).Include(x => x.Endereco).ToListAsync());
-        }
-
-        // GET: Fornecedor/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var fornecedor = await _context.Fornecedor
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (fornecedor == null)
-            {
-                return NotFound();
-            }
-
-            return View(fornecedor);
         }
 
         // GET: Fornecedor/Create
@@ -66,6 +50,25 @@ namespace CineManager.Controllers
             return View(fornecedor);
         }
 
+        // GET: Fornecedor/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedor.Include(x => x.Email).
+                Include(x => x.Telefone).Include(x => x.Endereco)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
         // GET: Fornecedor/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -74,7 +77,8 @@ namespace CineManager.Controllers
                 return NotFound();
             }
 
-            var fornecedor = await _context.Fornecedor.FindAsync(id);
+            var fornecedor = await _context.Fornecedor.Include(x => x.Email).
+                Include(x => x.Telefone).Include(x => x.Endereco).FirstOrDefaultAsync(x => x.Id == id);
             if (fornecedor == null)
             {
                 return NotFound();
--- CineManager/CineManager/Controllers/SalasController.cs
+++ CineManager/CineManager/Controllers/SalasController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CineManager.Data;
 using CineManager.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CineManager.Controllers
 {
+    [Authorize]
     public class SalasController : Controller
     {
         private readonly ApplicationDbContext _context;
