f6ebe31e8277bce1436e35dd9c1e577142c179d1
Pedro Henrique <2019100616@unibrasil.com.br>
2020-04-14 10:38:19 -0300

    Cor representativa

--- CineManager/CineManager/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CineManager/CineManager/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -48,9 +48,10 @@ namespace CineManager.Areas.Identity.Pages.Account
 
             [Required]
             [DataType(DataType.Password)]
+            [Display(Name = "Senha")]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar de mim?")]
             public bool RememberMe { get; set; }
         }
 
