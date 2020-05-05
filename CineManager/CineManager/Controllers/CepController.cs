01bc1d6d67e55351f429b9f8bb3b89a42d4f01e3
Pedro Henrique <2019100616@unibrasil.com.br>
2020-05-05 11:05:22 -0300

    Teste 1, retorno Exception na View Create Funcionario

---
+++ CineManager/CineManager/Controllers/CepController.cs
@@ -0,0 +1,26 @@
+﻿using CineManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace CineManager.Controllers
+{
+    public class CepController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Cep = Models.Cep.Busca("82840250");
+            return View();
+        }
+
+        public string Consulta(string cep)
+        {
+            var cepObj = Cep.Busca(cep);
+            return new JavaScriptSerializer().Serialize(cepObj);
+        }
+    }
+}
---
+++ CineManager/CineManager/wwwroot/js/consultaCep.js
@@ -0,0 +1,18 @@
+﻿
+jQuery(function ($) {
+    $("input[name='cep']").change(function () {
+        var cep_code = $(this).val();
+        if (cep_code.lenght <= 0) return;
+        $.get("https://ws.apicep.com/busca-cep/api/cep.json", { code: cep_code }, function (result) {
+            if (result.status != 1) {
+                alert(result.message || "houve um erro.");
+                return;
+            }
+            $("input[name='cep']").val(result.code);
+            $("input[name='estado']").val(result.state);
+            $("input[name='cidade']").val(result.city);
+            $("input[name='bairro']").val(result.district);
+            $("input[name='endereco']").val(result.address);
+        });
+    });
+});
