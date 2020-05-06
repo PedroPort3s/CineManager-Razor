1a7c0dd662186b409406a9a920334c189652de66
Pedro Henrique <2019100616@unibrasil.com.br>
2020-05-05 23:27:27 -0300

    Funcionando isso em

--- CineManager/CineManager/wwwroot/js/consultaCep.js
+++ CineManager/CineManager/wwwroot/js/consultaCep.js
@@ -2,17 +2,19 @@
 jQuery(function ($) {
     $("input[name='cep']").change(function () {
         var cep_code = $(this).val();
-        if (cep_code.lenght <= 0) return;
-        $.get("https://ws.apicep.com/busca-cep/api/cep.json", { code: cep_code }, function (result) {
-            if (result.status != 1) {
-                alert(result.message || "houve um erro.");
+        if (cep_code.length <= 0) return;
+        $.get("https://apps.widenet.com.br/busca-cep/api/cep.json", { code: cep_code }, function (result) {
+            if (result.status == 200) {
+                $("input[name='cep']").val(result.code);
+                $("input[name='estado']").val(result.state);
+                $("input[name='cidade']").val(result.city);
+                $("input[name='bairro']").val(result.district);
+                $("input[name='endereco']").val(result.address);
+            }
+            else {
+                alert(result.message || "Houve um erro.");
                 return;
             }
-            $("input[name='cep']").val(result.code);
-            $("input[name='estado']").val(result.state);
-            $("input[name='cidade']").val(result.city);
-            $("input[name='bairro']").val(result.district);
-            $("input[name='endereco']").val(result.address);
         });
     });
 });
